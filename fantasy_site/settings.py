44d3659c235169790f68578b090d40f2b69e53da
Nicholas Wilfong <madisonfc12@outlook.com>
2026-03-14 15:22:47 -0500

    changed header and made website Mobile friendl

--- fantasy_site/settings.py
+++ fantasy_site/settings.py
@@ -71,8 +71,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+"whitenoise.middleware.WhiteNoiseMiddleware",
 ]
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 ROOT_URLCONF = 'fantasy_site.urls'
 
 TEMPLATES = [
